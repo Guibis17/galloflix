16d3dd408d20d9fc2cdca5306392ee21c112f3d7
Guilherme Bispo e Gabriel <guilherme19.b07@gmail.com>
2023-05-16 08:35:16 -0300

    alterações códigos

--- GalloFlix/Models/Genre.cs
+++ GalloFlix/Models/Genre.cs
@@ -1,7 +1,9 @@
-namespace GalloFlix.Models
+namespace GalloFlix.Models;
+using System.ComponentModel.DataAnnotations;
 {
     public class Genre
     {
+        [Key]
         public byte Id {get; set;}
         public string Name {get; set;}
             
